2a79e275e118ded58aae5071b14d9de385271ef2
Muhammad Hamdi <mohamed.aboelnagd93@gmail.com>
2025-04-21 00:02:45 +0200

    registration & login is created

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule,
+    MessagesModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  name!: FormControl
+  email!: FormControl
+  password!: FormControl
+  rePassword!: FormControl
+  registrationForm!: FormGroup
 
+  constructor() {
+    this.initFormControls();
+    this.initFormGroup();
+  }
+
+  initFormControls(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]);
+    this.rePassword = new FormControl('', [
+      Validators.required,
+      this.passwordMatch(this.password),
+    ]);
+  }
+  initFormGroup(): void {
+    this.registrationForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      rePassword: this.rePassword,
+    });
+  }
+  passwordMatch(pass: AbstractControl): ValidatorFn {
+    return (rePass: AbstractControl): null | { [key: string]: boolean } => {
+      if (pass.value !== rePass.value || rePass.value === "") {
+        return { passNotMatch: true };
+      } else return null;
+    };
+  }
+  submit() {
+    // console.log(this.registrationForm.value);
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    } else {
+      this.registrationForm.markAllAsTouched();
+      Object.keys(this.registrationForm.controls).forEach((control) =>
+        this.registrationForm.controls[control].markAsDirty()
+      );
+    }
+  }
 }
